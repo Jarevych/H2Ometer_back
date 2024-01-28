8da4c8b44aa0afe853bd31858840e6b0723d8ac7
Yaroslav Grabovych <ygrabovych@gmail.com>
2024-01-28 14:10:03 +0200

    fix water rate

--- index.js
+++ index.js
@@ -5,21 +5,31 @@ const morgan = require('morgan')
 const dotenv = require('dotenv')
 const cors = require('cors')
 const path = require('path');
-const waterRate = require('./routes/')
+const waterRate = require('./routes/waterRate')
 
-
+const authRouter = require("./routes/auth");
 dotenv.config({
     path: path.resolve(__dirname, 'main.env')
   });
 
 connectDB();
 
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+app.use(morgan(formatsLogger));
 app.use(cors())
 
 app.use(morgan('dev'));
 app.use(express.json());
 
+app.use("/users", authRouter);
+app.use("/waterrate", waterRate)
 
-app.use('./api', waterRate)
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
+});
 
 module.exports = app
