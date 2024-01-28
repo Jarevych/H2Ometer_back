d850e2ec2218c7d933610d17c6e4f52bbc272901
Yaroslav Grabovych <ygrabovych@gmail.com>
2024-01-28 13:18:20 +0200

    use,api

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const morgan = require('morgan')
 const dotenv = require('dotenv')
 const cors = require('cors')
 const path = require('path');
+const waterRate = require('./routes/waterRate')
 
 
 dotenv.config({
@@ -18,5 +19,7 @@ app.use(cors())
 app.use(morgan('dev'));
 app.use(express.json());
 
+
+app.use('./api', waterRate)
 
 module.exports = app
