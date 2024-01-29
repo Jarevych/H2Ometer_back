fe52d317a79d6342f6eb72de2a68a817f2b76023
Alexandr150983 <alexandr150983@gmail.com>
2024-01-29 11:25:34 +0200

    add feature currentUser

--- routes/user.js
+++ routes/user.js
@@ -8,4 +8,6 @@ const { authenticate, upload } = require("../middlewares/");
 
 router.post("/avatar", authenticate, upload.single("avatar"), ctrl.addAvatar);
 
+router.get("/current", authenticate, ctrl.getCurrent);
+
 module.exports = router;
