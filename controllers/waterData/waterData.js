4c44cddcff60dde3f7611ae14cb75cc2e00afcb2
Yaroslav Grabovych <ygrabovych@gmail.com>
2024-01-31 19:41:15 +0200

    add water data search

--- controllers/waterData/waterData.js
+++ controllers/waterData/waterData.js
@@ -1,26 +1,60 @@
+// const mongoose = require('mongoose');
+
 const Water = require("../../models/water");
 
 const waterData = async (req, res) => {
   try {
     const { day, month } = req.query;
-    const { ownerId } = req.params;
+    const { _id: ownerId } = req.user || {};
     const today = new Date().getDate();
     // const currentMonth = new Date().getMonth() + 1;
+    console.log('user', req.user)
+    const startOfMonth = new Date(`2024-${month}-01T00:00:00Z`);
+    // const nextMonth = month === 12 ? 1 : month + 1;
+    const endOfMonth = new Date(startOfMonth);
+    endOfMonth.setMonth(endOfMonth.getMonth() + 1);
+    const selectedDay = new Date(`2024-${month}-${day}T00:00:00Z`);
 
+    console.log("startOfMonth:", startOfMonth);
+    console.log("endOfMonth:", endOfMonth);
+    console.log("selectedDay:", selectedDay);
+    console.log("ownerId", ownerId);
     const dayValue = day.toLowerCase() === "today" ? today : parseInt(day);
     const monthValue =
       day.toLowerCase() === "currentMonth" ? today : parseInt(month);
 
     const waterData = await Water.find({
-      ownerId: ownerId,
-      day: dayValue,
-      month: monthValue,
+      ownerId,
+      $or: [
+        {
+          createdAt: {
+            $gte: startOfMonth,
+            $lt: endOfMonth,
+          },
+        },
+        {
+          createdAt: {
+            $gte: selectedDay,
+            $lt: new Date(selectedDay.getTime() + 24 * 60 * 60 * 1000),
+          },
+        },
+      ],
     });
+
+    // const dayValue = day.toLowerCase() === "today" ? today : parseInt(day);
+    // const monthValue =
+    //   day.toLowerCase() === "currentMonth" ? today : parseInt(month);
+
+    // const waterData = await Water.find({
+    //   ownerId: ownerId,
+    //   day: dayValue,
+    //   month: monthValue,
+    // });
 
     if (waterData.length === 0) {
       return res.status(404).json({ message: "No data found" });
     }
-
+    console.log(waterData);
     res.json({
       ownerId: ownerId,
       day: dayValue,
