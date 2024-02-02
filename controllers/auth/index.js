85e899fc5629efd88a5d9a5576bfb1105b319ad3
Alexandr150983 <alexandr150983@gmail.com>
2024-02-02 14:47:55 +0200

    add feature verify email

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -3,6 +3,8 @@ const { login } = require("./login");
 const { logout } = require("./logout");
 const { requestPasswordReset } = require("./requestPasswordReset");
 const { resetPassword } = require("./resetPassword");
+const { verifyEmail } = require("./verifyEmail");
+const { resendVerifyEmail } = require("./resendVerifyEmail.js");
 
 module.exports = {
   register,
@@ -10,4 +12,6 @@ module.exports = {
   logout,
   requestPasswordReset,
   resetPassword,
+  verifyEmail,
+  resendVerifyEmail,
 };
---
+++ controllers/auth/resendVerifyEmail.js
@@ -0,0 +1,37 @@
+const { HttpError, ctrlWrapper } = require("../../helpers");
+const { User } = require("../../models/user");
+const { sendEmail } = require("../../services");
+
+
+const resendVerifyEmail = async (req, res) => {
+  const { email } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw HttpError(400, "missing required field email");
+  }
+  if (user.verify) {
+    throw HttpError(400, "Verification has already been passed");
+  }
+
+  const verifyEmail = {
+    to: email,
+    subject: "Verify email",
+    html: `
+    <p>Hello,</p>
+    <p>Thank you for registering. Please click the link below to verify your email address:</p>
+    <a target="_blank" href="${process.env.BASE_URL_FRONT}/users/verify/${user.verificationToken}">Verify Email</a>
+    <p>If you did not create an account, no further action is required.</p>
+    <p>Regards,<br>Your Team</p>
+  `,
+  };
+
+  await sendEmail(verifyEmail);
+
+  res.json({
+    message: "Verification email sent",
+  });
+};
+
+module.exports = {
+  resendVerifyEmail: ctrlWrapper(resendVerifyEmail),
+};
