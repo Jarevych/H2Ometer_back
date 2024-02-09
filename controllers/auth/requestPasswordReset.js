c6bc1dc46f61ef07cf4ab509b1b357d81a8ea37f
Alexandr150983 <alexandr150983@gmail.com>
2024-02-09 09:10:41 +0200

    fix send email list

--- controllers/auth/requestPasswordReset.js
+++ controllers/auth/requestPasswordReset.js
@@ -26,18 +26,20 @@ const requestPasswordReset = async (req, res) => {
 
   const emailData = {
     to: email,
-    subject: "Password Reset",
+    subject: "H2Ometer Password Reset Instructions",
     html: `
     <h1>Password Reset Instructions</h1>
-    <p>Dear user,</p>
-    <p>You have requested to reset your password. To do so, please follow the instructions below:</p>
+    <p>Dear H2Ometer User,</p>
+    <p>We received a request to reset the password for your H2Ometer account. If you made this request, please follow the instructions below to set a new password:</p>
     <ol>
       <li>Click on the following link to reset your password:</li>
       <li><a href="${resetLink}">Reset Your Password</a></li>
     </ol>
-    <p>If you did not request a password reset, please ignore this email.</p>
+    <p>Please note that this link will expire in 24 hours for your security.</p>
+    <p>If you did not request to reset your password, you can safely ignore this email. Your account password will not change.</p>
+    <p>Should you have any questions or require further assistance, please do not hesitate to contact our support team.</p>
     <p>Best regards,</p>
-    <p>Your Website Team</p>
+    <p>The H2Ometer Team</p>
   `,
   };
 
